609819cb14a2b4b42c07e4daf3fcfbabe9fe9017
Trinkles <109135115+trinkles@users.noreply.github.com>
2023-06-04 23:43:18 +0800

    Update Spinely.ino

--- Spinely.ino
+++ Spinely.ino
@@ -5,11 +5,25 @@ const char* ssid = "YourWiFiSSID";
 const char* password = "YourWiFiPassword";
 const char* serverIP = "192.168.1.100";
 const int serverPort = 80;
-const char* endpoint = "/functions.php";
+const char* deviceEndpoint = "/device.php";
+const char* sensorDataEndpoint = "/receive_sensor_data.php";
+const char* anglesEndpoint = "/angles.php";
 const char* deviceName = "MyDevice";
 const char* deviceIP = "192.168.1.200";
 
 bool isOperating = false;
+
+unsigned long previousTime = 0;
+const unsigned long interval = 1000;
+int elapsedTime = 0;
+
+const int flexPins[] = {36, 39, 32, 33, 34, 35, 25};
+const int numFlexPins = sizeof(flexPins) / sizeof(flexPins[0]);
+const int motor = 12;
+
+float sensorValues[numFlexPins];
+int minFlexValues[numFlexPins];
+int angles[numFlexPins];
 
 void setup() {
   Serial.begin(115200);
@@ -19,20 +33,15 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Connected to WiFi");
-  
+  fetchMinFlexValues();
   sendDeviceInfo();
-
   isOperating = false;
 }
 
 void loop() {
-  // Check if device is operating or waiting for command
   if (isOperating) {
-    // Code for operating the device goes here
-    // Execute the appropriate function based on the received command
-    if (receivedCommand == "calibration") {
+    if (receivedCommand == "calibration" || receivedCommand.startsWith("calibrate_existing") || receivedCommand.startsWith("calibrate_now")) {
       performCalibration();
     } else if (receivedCommand == "monitoring") {
       startMonitoring();
@@ -40,75 +49,174 @@ void loop() {
       turnOnMotor();
     }
 
-    // After executing the command, set the device back to waiting state
     isOperating = false;
   } else {
-    // Code for waiting for command goes here
-    // Check for new commands from the server
     checkForCommands();
   }
 }
 
 void sendDeviceInfo() {
   HTTPClient http;
-  
-  String url = "http://" + String(serverIP) + ":" + String(serverPort) + endpoint;
-  
+
+  String url = "http://" + String(serverIP) + ":" + String(serverPort) + deviceEndpoint;
   String data = "deviceName=" + String(deviceName) + "&deviceIP=" + String(deviceIP) + "&status=" + (isOperating ? "operating" : "waiting");
-  
   http.begin(url);
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpResponseCode = http.POST(data);
-  
+
   if (httpResponseCode == HTTP_CODE_OK) {
     Serial.println("Device information sent successfully");
   } else {
     Serial.print("Failed to send device information. Error code: ");
     Serial.println(httpResponseCode);
   }
-  
+
   http.end();
 }
 
 void checkForCommands() {
-  // Create the HTTP object
   HTTPClient http;
 
-  // Construct the URL
-  String url = "http://" + String(serverIP) + ":" + String(serverPort) + endpoint;
+  String url = "http://" + String(serverIP) + ":" + String(serverPort) + deviceEndpoint;
 
-  // Send the GET request to check for commands
   http.begin(url);
   int httpResponseCode = http.GET();
 
-  // Check if a command is received
   if (httpResponseCode == HTTP_CODE_OK) {
     String receivedCommand = http.getString();
     Serial.print("Received command: ");
     Serial.println(receivedCommand);
 
-    // Set the device to operating state
     isOperating = true;
+
+    elapsedTime = 0;
   } else {
     Serial.print("Failed to check for commands. Error code: ");
     Serial.println(httpResponseCode);
   }
 
-  // Close the connection
   http.end();
 }
 
 void performCalibration() {
   Serial.println("Performing calibration...");
-  // Code for calibration goes here
+
+  while (elapsedTime < 10000) {
+    unsigned long currentTime = millis();
+    if (currentTime - previousTime >= interval) {
+      previousTime = currentTime;
+
+      readSensorValues();
+      mapFlexValuesToAngles();
+      sendAnglesToPHP();
+
+      elapsedTime += interval;
+    }
+  }
+
+  Serial.println("Calibration complete");
+}
+
+void readSensorValues() {
+  for (int i = 0; i < numFlexPins; i++) {
+    sensorValues[i] = analogRead(flexPins[i]);
+  }
+}
+
+void mapFlexValuesToAngles() {
+  for (int i = 0; i < numFlexPins; i++) {
+    angles[i] = map(sensorValues[i], minFlexValues[i], 1023, 0, 180);
+  }
+}
+
+void sendAnglesToPHP() {
+  HTTPClient http;
+
+  String url = "http://" + String(serverIP) + ":" + String(serverPort) + anglesEndpoint;
+
+  String data = "upperBack=" + String(angles[0]) +
+                "&middleBack=" + String(angles[1]) +
+                "&lowerBack=" + String(angles[2]) +
+                "&leftShoulder=" + String(angles[3]) +
+                "&rightShoulder=" + String(angles[4]) +
+                "&leftSide=" + String(angles[5]) +
+                "&rightSide=" + String(angles[6]);
+
+  http.begin(url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST(data);
+
+  if (httpResponseCode == HTTP_CODE_OK) {
+    Serial.println("Angle data sent successfully");
+  } else {
+    Serial.print("Failed to send angle data. Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
 }
 
 void startMonitoring() {
   Serial.println("Starting monitoring...");
-  // Code for monitoring goes here
+
+  while (isOperating) {
+    unsigned long currentTime = millis();
+    if (currentTime - previousTime >= interval) {
+      previousTime = currentTime;
+
+      readSensorValues();
+      mapFlexValuesToAngles();
+      sendAnglesToPHP();
+
+      elapsedTime += interval;
+    }
+  }
 }
 
 void turnOnMotor() {
   Serial.println("Turning on motor...");
-  // Code to turn on the motor goes here
+  digitalWrite(motor, HIGH);
+  delay(2000);
+  digitalWrite(motor, LOW);
+  Serial.println("Motor turned off");
+}
+
+void fetchMinFlexValues() {
+  HTTPClient http;
+  String url = "http://" + String(serverIP) + ":" + String(serverPort) + "/fetch_min_flex_values.php";
+  http.begin(url);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == HTTP_CODE_OK) {
+    String response = http.getString();
+    parseMinFlexValues(response);
+  } else {
+    Serial.print("Failed to fetch minimum flex values. Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+}
+
+void parseMinFlexValues(String response) {
+  int index = 0;
+  while (response.length() > 0) {
+    int commaIndex = response.indexOf(',');
+    if (commaIndex != -1) {
+      String valueStr = response.substring(0, commaIndex);
+      response = response.substring(commaIndex + 1);
+      minFlexValues[index] = valueStr.toInt();
+    } else {
+      minFlexValues[index] = response.toInt();
+      response = "";
+    }
+
+    index++;
+  }
+
+  Serial.println("Minimum Flex Values:");
+  for (int i = 0; i < numFlexPins; i++) {
+    Serial.print("Pin ");
+    Serial.print(flexPins[i]);
+    Serial.print(": ");
+    Serial.println(minFlexValues[i]);
+  }
 }
