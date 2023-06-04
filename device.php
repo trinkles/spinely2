ce6457c92b38b709d510c239ca7d72ec802d150a
Trinkles <109135115+trinkles@users.noreply.github.com>
2023-06-05 01:44:39 +0800

    Update device.php

--- device.php
+++ device.php
@@ -1,91 +1,132 @@
 <?php
-
-// Include the database connection file
 require_once 'database.php';
 
-// Function to handle device information
 function handleDeviceInformation($deviceName, $deviceIP, $status) {
-  // Generate a random device ID
-  $deviceID = generateDeviceID();
+    $deviceConnected = false;
+    $deviceID = generateDeviceID();
+    $timestamp = date('Y-m-d H:i:s');
+    saveDeviceInfo($deviceID, $deviceName, $deviceIP, $status, $timestamp);
 
-  // Get the current timestamp
-  $timestamp = date('Y-m-d H:i:s');
-
-  // Save the device information to the database
-  saveDeviceInfo($deviceID, $deviceName, $deviceIP, $status, $timestamp);
-
-  // Display the connected devices
-  displayConnectedDevices();
+    if ($status == 'connected') {
+        $deviceConnected = true;
+        header('Location: calibration.php');
+        exit();
+    }
 }
 
-// Generate a random device ID
-function generateDeviceID() {
-  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  $deviceID = '';
-  $length = 10;
+function displayLatestConnectedDevice() {
+    global $conn;
 
-  for ($i = 0; $i < $length; $i++) {
-    $deviceID .= $characters[rand(0, strlen($characters) - 1)];
-  }
+    $query = "SELECT * FROM devices ORDER BY timestamp_connected DESC LIMIT 1";
+    $result = mysqli_query($conn, $query);
 
-  return $deviceID;
-}
-
-// Save the device information to the database
-function saveDeviceInfo($deviceID, $deviceName, $deviceIP, $status, $timestamp) {
-  global $conn; // Access the global database connection variable
-
-  // Insert the device information into the table
-  $query = "INSERT INTO devices (device_id, device_name, device_ip, status, timestamp_connected) 
-            VALUES ('$deviceID', '$deviceName', '$deviceIP', '$status', '$timestamp')";
-  $result = mysqli_query($conn, $query);
-
-  if (!$result) {
-    die('Failed to save device information: ' . mysqli_error($conn));
-  }
-}
-
-// Display the connected devices
-function displayConnectedDevices() {
-  global $conn; // Access the global database connection variable
-
-  // Fetch the connected devices from the database
-  $query = "SELECT * FROM devices";
-  $result = mysqli_query($conn, $query);
-
-  if (!$result) {
-    die('Failed to fetch connected devices: ' . mysqli_error($conn));
-  }
-
-  // Check if there are any connected devices
-  if (mysqli_num_rows($result) > 0) {
-    echo "<h3>Connected Devices:</h3>";
-    echo "<table>";
-    echo "<tr><th>Device ID</th><th>Device Name</th><th>Device IP</th><th>Status</th></tr>";
-
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<tr>";
-      echo "<td>" . $row['device_id'] . "</td>";
-      echo "<td>" . $row['device_name'] . "</td>";
-      echo "<td>" . $row['device_ip'] . "</td>";
-      echo "<td>" . $row['status'] . "</td>";
-      echo "</tr>";
+    if (!$result) {
+        die('Failed to fetch connected device: ' . mysqli_error($conn));
     }
 
-    echo "</table>";
-  } else {
-    echo "<p>No connected devices</p>";
-  }
+    $device = mysqli_fetch_assoc($result);
+
+    return $device;
 }
 
+function generateDeviceID() {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $deviceID = '';
+    $length = 10;
 
-// Handle the device information received from Arduino
-if (isset($_POST['deviceName']) && isset($_POST['deviceIP']) && isset($_POST['status'])) {
-  $deviceName = $_POST['deviceName'];
-  $deviceIP = $_POST['deviceIP'];
-  $status = $_POST['status'];
+    for ($i = 0; $i < $length; $i++) {
+        $deviceID .= $characters[rand(0, strlen($characters) - 1)];
+    }
 
-  handleDeviceInformation($deviceName, $deviceIP, $status);
+    return $deviceID;
 }
 
+function saveDeviceInfo($deviceID, $deviceName, $deviceIP, $status, $timestamp) {
+    global $conn;
+
+    $deviceID = mysqli_real_escape_string($conn, $deviceID);
+    $deviceName = mysqli_real_escape_string($conn, $deviceName);
+    $deviceIP = mysqli_real_escape_string($conn, $deviceIP);
+    $status = mysqli_real_escape_string($conn, $status);
+    $timestamp = mysqli_real_escape_string($conn, $timestamp);
+
+    $query = "INSERT INTO devices (device_id, device_name, device_ip, status, timestamp_connected) 
+              VALUES ('$deviceID', '$deviceName', '$deviceIP', '$status', '$timestamp')";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die('Failed to save device information: ' . mysqli_error($conn));
+    }
+}
+
+if (isset($_POST['deviceName'], $_POST['deviceIP'], $_POST['status'])) {
+    $deviceName = $_POST['deviceName'];
+    $deviceIP = $_POST['deviceIP'];
+    $status = $_POST['status'];
+
+    handleDeviceInformation($deviceName, $deviceIP, $status);
+}
+
+// Fetch the latest connected device
+$latestDevice = displayLatestConnectedDevice();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Device Information</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        function updateDeviceInformation() {
+            $.ajax({
+                url: '',
+                dataType: "json",
+                success: function (data) {
+                    if (data) {
+                        $('#deviceName').text(data.device_name);
+                        $('#deviceIP').text(data.device_ip);
+                        $('#status').text(data.status);
+                        <?php if (isset($latestDevice['timestamp'])) : ?>
+                            $('#timestamp').text(data.timestamp);
+                        <?php endif; ?>
+
+                        if (data.status === 'waiting') {
+                            $('#connectionButton').show();
+                        } else {
+                            $('#connectionButton').hide();
+                        }
+                    } else {
+                        $('#deviceName').text('');
+                        $('#deviceIP').text('');
+                        $('#status').text('');
+                        <?php if (isset($latestDevice['timestamp'])) : ?>
+                            $('#timestamp').text('');
+                        <?php endif; ?>
+                        $('#connectionButton').hide();
+                    }
+                }
+            });
+        }
+
+        $(document).ready(function () {
+            updateDeviceInformation();
+            setInterval(updateDeviceInformation, 5000);
+        });
+    </script>
+</head>
+<body>
+    <h1>Device Information</h1>
+
+    <h2>Latest Connected Device:</h2>
+    <p>Device Name: <span id="deviceName"><?php echo $latestDevice['device_name']; ?></span></p>
+    <p>Device IP: <span id="deviceIP"><?php echo $latestDevice['device_ip']; ?></span></p>
+    <p>Status: <span id="status"><?php echo $latestDevice['status']; ?></span></p>
+    <?php if (isset($latestDevice['timestamp'])) : ?>
+        <p>Timestamp: <span id="timestamp"><?php echo $latestDevice['timestamp']; ?></span></p>
+    <?php endif; ?>
+
+    <?php if ($latestDevice['status'] === 'waiting') : ?>
+        <button id="connectionButton" onclick="location.href='calibration.php';">Go to Calibration</button>
+    <?php endif; ?>
+</body>
+</html>
