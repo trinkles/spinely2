43fd15b86e6b754f54cdbfb2ea47cc18f5f74a7d
Trinkles <109135115+trinkles@users.noreply.github.com>
2023-06-04 14:32:00 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection details
+$servername = "localhost"; // Replace with your server name
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$dbname = "spinely_test2"; // Replace with your database name
+
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
