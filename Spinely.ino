5bc89c5294aa098e810339a3d2917fd4a9bcb001
Trinkles <109135115+trinkles@users.noreply.github.com>
2023-06-04 15:03:48 +0800

    Update Spinely.ino
    When arduino code receives command from php, determine which function (calibration, monitoring, turn_on_motor). When arduino code receives function it is "operating", otherwise it is "waiting".
    
    this Arduino device starts off in this chill state where it's just waiting around, not really doing anything important (aka the "isOperating = false" state). Every now and then, the device wakes up and goes, "Hey, is there anything new from that PHP server?" It does this using "checkForCommands()".
    
    If the device actually gets a command from the server, it sets its state to "isOperating = true" (meaning it's ready to roll). Based on the command it received, the device picks the right function to execute.

--- Spinely.ino
+++ Spinely.ino
@@ -24,15 +24,28 @@ void setup() {
   
   sendDeviceInfo();
 
-  isOperating = true;
+  isOperating = false;
 }
 
 void loop() {
   // Check if device is operating or waiting for command
   if (isOperating) {
     // Code for operating the device goes here
+    // Execute the appropriate function based on the received command
+    if (receivedCommand == "calibration") {
+      performCalibration();
+    } else if (receivedCommand == "monitoring") {
+      startMonitoring();
+    } else if (receivedCommand == "turn_on_motor") {
+      turnOnMotor();
+    }
+
+    // After executing the command, set the device back to waiting state
+    isOperating = false;
   } else {
     // Code for waiting for command goes here
+    // Check for new commands from the server
+    checkForCommands();
   }
 }
 
@@ -55,4 +68,47 @@ void sendDeviceInfo() {
   }
   
   http.end();
+}
+
+void checkForCommands() {
+  // Create the HTTP object
+  HTTPClient http;
+
+  // Construct the URL
+  String url = "http://" + String(serverIP) + ":" + String(serverPort) + endpoint;
+
+  // Send the GET request to check for commands
+  http.begin(url);
+  int httpResponseCode = http.GET();
+
+  // Check if a command is received
+  if (httpResponseCode == HTTP_CODE_OK) {
+    String receivedCommand = http.getString();
+    Serial.print("Received command: ");
+    Serial.println(receivedCommand);
+
+    // Set the device to operating state
+    isOperating = true;
+  } else {
+    Serial.print("Failed to check for commands. Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Close the connection
+  http.end();
+}
+
+void performCalibration() {
+  Serial.println("Performing calibration...");
+  // Code for calibration goes here
+}
+
+void startMonitoring() {
+  Serial.println("Starting monitoring...");
+  // Code for monitoring goes here
+}
+
+void turnOnMotor() {
+  Serial.println("Turning on motor...");
+  // Code to turn on the motor goes here
 }
