fe20a992fd4bb61a8f007007cd05cc42af79deaa
Trinkles <109135115+trinkles@users.noreply.github.com>
2023-06-04 21:04:35 +0800

    Update choices.php

--- choices.php
+++ choices.php
@@ -1,74 +1,29 @@
 <?php
-// Include the database credentials
-require 'database.php';
+// Include the database.php file
+require_once 'database.php';
 
-// Arduino IP address and port
-$arduinoIP = '192.168.1.100';
-$arduinoPort = 80;
+// Function to retrieve existing postures from the calibration table
+function getExistingPostures()
+{
+    global $conn;
 
-// Command to send to Arduino
-$command = '';
+    $query = "SELECT DISTINCT posture_name FROM calibration";
+    $result = mysqli_query($conn, $query);
 
-// Handle the button clicks
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['calibrate_existing'])) {
-        // Calibrate existing posture
-        $postureName = $_POST['existing_posture'];
-        $command = "calibrate_existing|$postureName";
-    } elseif (isset($_POST['calibrate_new'])) {
-        // Calibrate new posture
-        $postureName = $_POST['new_posture'];
-        $command = "calibrate_new|$postureName";
-    } elseif (isset($_POST['monitor_posture'])) {
-        // Monitor posture
-        $command = "monitor_posture";
-    }
-}
-
-// Send command to Arduino
-if (!empty($command)) {
-    // Create a cURL resource
-    $ch = curl_init();
-
-    // Set the cURL options
-    curl_setopt($ch, CURLOPT_URL, "http://{$arduinoIP}:{$arduinoPort}/");
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ['command' => $command]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Execute the cURL request
-    $response = curl_exec($ch);
-
-    // Check for errors
-    if ($response === false) {
-        die('Failed to send command to Arduino: ' . curl_error($ch));
+    if (!$result) {
+        die('Failed to retrieve existing postures: ' . mysqli_error($conn));
     }
 
-    // Close the cURL resource
-    curl_close($ch);
+    $postures = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $postures[] = $row['posture_name'];
+    }
 
-    // Display the response from Arduino
-    echo 'Command sent to Arduino: ' . $command;
+    return $postures;
 }
 
-// Retrieve existing postures from the database
-$existingPostures = [];
-$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-if (!$conn) {
-    die('Failed to connect to the database: ' . mysqli_connect_error());
-}
-
-$query = "SELECT DISTINCT posture_name FROM calibration";
-$result = mysqli_query($conn, $query);
-if (!$result) {
-    die('Failed to retrieve existing postures: ' . mysqli_error($conn));
-}
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $existingPostures[] = $row['posture_name'];
-}
-
-mysqli_close($conn);
+// Retrieve existing postures
+$existingPostures = getExistingPostures();
 
 ?>
 
@@ -94,3 +49,30 @@ mysqli_close($conn);
     <h4>Monitor Posture:</h4>
     <button type="submit" name="monitor_posture">Start Monitoring</button>
 </form>
+
+<?php
+// Handle the button clicks
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['calibrate_existing'])) {
+        // Calibrate existing posture
+        $postureName = $_POST['existing_posture'];
+        $command = "calibrate_existing|$postureName";
+        // Redirect to calibrate.php with the selected posture as a query parameter
+        header("Location: calibrate.php?posture=$postureName");
+        exit;
+    } elseif (isset($_POST['calibrate_new'])) {
+        // Calibrate new posture
+        $postureName = $_POST['new_posture'];
+        $command = "calibrate_new|$postureName";
+        // Redirect to calibrate.php with the new posture as a query parameter
+        header("Location: calibrate.php?posture=$postureName");
+        exit;
+    } elseif (isset($_POST['monitor_posture'])) {
+        // Monitor posture
+        $command = "monitor_posture";
+        // Redirect to monitor.php
+        header("Location: monitor.php");
+        exit;
+    }
+}
+?>
